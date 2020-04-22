eee34c27a09df26e0c2ea880eb2453cf40cef362
ljh0411 <ljhpok1145@naver.com>
2020-04-22 12:17:36 +0900

    수정 이준호

--- src/jihee/test.java
+++ src/jihee/test.java
@@ -2,7 +2,7 @@ package jihee;
 
 public class test {
 public static void main(String[] args) {
-	System.out.println("test22222222222222222222222222");
+	System.out.println("test222222222222");
 
 }
 }
